9c17dfd0b521ffb0f680b9620bdeba983ccec557
57336_Sumeet <mulaysumeet28@gmail.com>
2023-08-12 12:15:42 +0530

    first commit

---
+++ SpringBoot_GITDEMO/src/main/java/com/boot/gitdemo/SpringBootGitdemoApplication.java
@@ -0,0 +1,13 @@
+package com.boot.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGitdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGitdemoApplication.class, args);
+	}
+
+}
---
+++ SpringBoot_GITDEMO/src/test/java/com/boot/gitdemo/SpringBootGitdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boot.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootGitdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
